d75c6217466fc7f2045e4240682319877aa59d57
TCTrung <trungtrinh1325@gmail.com>
2023-09-14 21:24:30 +0700

    hi

---
+++ my.js
@@ -0,0 +1,25 @@
+function result() {
+    let month = +document.getElementById('month').value;
+    switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                document.getElementById('ketqua').innerHTML = 'Thang ' + month + ' co 31 ngay';
+            break;
+            case 2:
+                document.getElementById('ketqua').innerHTML = 'Thang 2 co 28 hoac 29 ngay';
+            break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                document.getElementById('ketqua').innerHTML = 'Thang ' + month + ' co 30 ngay';
+            break;
+
+    }
+
+}
